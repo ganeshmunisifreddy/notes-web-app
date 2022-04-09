e37408fb760879eb6ad64b680418ad4bf599e17d
Ganesh <ganeshmunisif@gmail.com>
2022-04-09 23:27:45 +0530

    Next PWA

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development",
+  },
+  reactStrictMode: true,
+});
---
+++ pages/_offline.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const OfflinePage = (props: Props) => {
+  return <div>OfflinePage</div>;
+};
+
+export default OfflinePage;
